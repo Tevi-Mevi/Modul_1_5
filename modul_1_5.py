747d9479972c4dd50ab4e8c0caff3cc7375a26c7
Tatevik <tatevik9330@gmail.com>
2024-11-30 16:00:33 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,13 @@
+name ="example"
+print(name[0])
+print(name[-1])
+print(name[4:])
+print(name[::-1])
+print(name[1:6:2])
+
+example ="Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
